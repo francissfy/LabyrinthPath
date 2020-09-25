aac49905e4c6e340d68c3a1d985d4b78673b8dd5
Shen Feiyu <heroicbig@126.com>
2020-09-25 11:01:43 +0800

    Initial Commit

---
+++ MF_ZMT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MF_ZMT
+//
+//  Created by Francis on 2020/9/25.
+//  Copyright © 2020 Francis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
